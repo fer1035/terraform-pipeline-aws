9e34830577271dff34063c2f52d4bcc57b32ef39
Ahmad Ferdaus Abd Razak <ahmad.ferdaus.abd.razak@ni.com>
2021-12-31 11:56:59 +0800

    Updated component descriptions.

--- modules/security_demo/api.tf
+++ modules/security_demo/api.tf
@@ -18,7 +18,8 @@ resource "aws_api_gateway_rest_api" "api" {
   #     }
   #   }
   # })
-  name = var.api_name
+  name        = var.api_name
+  description = var.api_description
   endpoint_configuration {
     types = ["REGIONAL"]  # | EDGE
   }

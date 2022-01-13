f4dcd4563b95b80056b172eba1ef61d47fbbb613
Ahmad Ferdaus Abd Razak <ahmad.ferdaus.abd.razak@ni.com>
2022-01-13 15:57:38 +0800

    Moved API stage into deployment definition.

--- modules/security_demo/api.tf
+++ modules/security_demo/api.tf
@@ -97,6 +97,7 @@ resource "aws_api_gateway_integration_response" "options" {
 
 resource "aws_api_gateway_deployment" "deployment" {
   rest_api_id = aws_api_gateway_rest_api.api.id
+  stage_name  = "test"
 
   triggers = {
     # NOTE: The configuration below will satisfy ordering considerations,
@@ -121,11 +122,11 @@ resource "aws_api_gateway_deployment" "deployment" {
   }
 }
 
-resource "aws_api_gateway_stage" "stage" {
-  deployment_id = aws_api_gateway_deployment.deployment.id
-  rest_api_id   = aws_api_gateway_rest_api.api.id
-  stage_name    = "test"
-}
+# resource "aws_api_gateway_stage" "stage" {
+#   deployment_id = aws_api_gateway_deployment.deployment.id
+#   rest_api_id   = aws_api_gateway_rest_api.api.id
+#   stage_name    = "test"
+# }
 
 resource "aws_api_gateway_method_settings" "all" {
   rest_api_id = aws_api_gateway_rest_api.api.id

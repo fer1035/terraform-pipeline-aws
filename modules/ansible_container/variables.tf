57df21daaa22bb0523d71398f705316746566376
Ahmad Ferdaus Abd Razak <ahmad.ferdaus.abd.razak@ni.com>
2022-03-09 19:54:24 +0800

    Updated ECS definition.

--- modules/ansible_container/variables.tf
+++ modules/ansible_container/variables.tf
@@ -24,3 +24,23 @@ variable "force_new" {
     type    = bool
     default = false
 }
+
+variable "subnet_1" {
+    type    = string
+    default = "subnet-072107252df3c7e87"
+}
+
+variable "subnet_2" {
+    type    = string
+    default = "subnet-03b71dab2f9a1bb6f"
+}
+
+variable "security_group" {
+    type    = string
+    default = "sg-05bd0031419bb91c5"
+}
+
+variable "public_ip" {
+    type    = bool
+    default = true
+}

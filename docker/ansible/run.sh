8c7c5deb8e08628814c73a0cc6bbb81690de5298
Ahmad Ferdaus Abd Razak <ahmad.ferdaus.abd.razak@ni.com>
2022-03-10 16:43:04 +0800

    Updated Dockerfile and credentials.

--- docker/ansible/run.sh
+++ docker/ansible/run.sh
@@ -6,7 +6,7 @@ export PUB="PUBKEY"
 
 # Install SSH.
 apt-get update
-apt-get install openssh python3 -y 
+apt-get install openssh-client curl python3 -y
 
 # Create credentials.
 mkdir -p ~/.ssh
@@ -15,7 +15,19 @@ echo "$PUB" > ~/.ssh/id_rsa.pub
 sed -i 's/\\n/\n/g' ~/.ssh/id_rsa
 chmod 600 ~/.ssh/id_rsa*
 
-pip3 install ansible
+# Install Ansible.
+python3 -m pip install --upgrade pip
+python3 -m pip install ansible
 
+# Run Ansible and collect outputs.
 cd /ansible
-ansible-playbook playbook.yml -i inventory
+ansible-playbook playbook.yml -i inventory > log.txt
+
+# Set output for Teams.
+WEBHOOK="WEBHOOK_URL"
+TITLE="Ansible Container Output"
+MESSAGE=$(cat log.txt | sed 's/"/\"/g' | sed "s/'/\'/g")
+JSON="{\"title\": \"${TITLE}\", \"text\": \"${MESSAGE}\"}"
+
+# Send output to Teams.
+curl -H "Content-Type: application/json" -d "${JSON}" "${WEBHOOK_URL}"

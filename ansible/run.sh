0c682ad9b9e6c395ac2272fe0e8dea837249bab0
Ahmad Ferdaus Abd Razak <ahmad.ferdaus.abd.razak@ni.com>
2022-03-21 15:34:35 +0800

    Updated.

--- ansible/run.sh
+++ ansible/run.sh
@@ -9,4 +9,4 @@ MESSAGE="$(cat log.txt)"
 MESSAGE="${MESSAGE//$'\n'/<br>}"
 
 # Send output to Teams.
-curl -H "Content-Type: application/json" -d "{\"@context\": \"http://schema.org/extensions\",\"@type\": \"MessageCard\", \"title\":\"ANSIBLE_TITLE\", \"text\": \"${MESSAGE}\"}" "ANSIBLE_WEBHOOK_URL"
+curl -X POST -H "Content-Type: application/json" -d "{\"@context\": \"http://schema.org/extensions\",\"@type\": \"MessageCard\", \"title\":\"ANSIBLE_TITLE\", \"text\": \"${MESSAGE}\"}" "ANSIBLE_WEBHOOK_URL"

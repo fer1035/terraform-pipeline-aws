5881e8515d3658778178ed2e046953e485b3b4a1
Ahmad Ferdaus Abd Razak <ahmad.ferdaus.abd.razak@ni.com>
2022-03-21 14:47:27 +0800

    Updated Ansible run commands.

--- ansible/run.sh
+++ ansible/run.sh
@@ -2,12 +2,10 @@
 
 # Ansible command.
 ANSIBLE_CMD > log.txt
-MESSAGE="$(cat log.txt)"
 
-# Set output for Teams.
-TITLE="ANSIBLE_TITLE"
-JSON="{\"title\": \"${TITLE}\", \"text\": \"${MESSAGE}\"}"
+# Format output structure.
+MESSAGE="$(cat log.txt)"
+MESSAGE="${MESSAGE//$'\n'/<br>}"
 
 # Send output to Teams.
-# curl -H "Content-Type: application/json" -d "${JSON}" "ANSIBLE_WEBHOOK_URL"
-curl -H 'Content-Type: application/json' -d '{"@context": "http://schema.org/extensions","@type": "MessageCard", "title":"ANSIBLE_TITLE", "text": "$(cat log.txt)"}' "ANSIBLE_WEBHOOK_URL"
+curl -H "Content-Type: application/json" -d "{\"@context\": \"http://schema.org/extensions\",\"@type\": \"MessageCard\", \"title\":\"ANSIBLE_TITLE\", \"text\": \"${MESSAGE}\"}" "ANSIBLE_WEBHOOK_URL"

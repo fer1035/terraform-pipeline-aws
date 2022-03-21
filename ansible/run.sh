642776f5de8f97ce32020ed89aa2b1c59ae839dd
Ahmad Ferdaus Abd Razak <ahmad.ferdaus.abd.razak@ni.com>
2022-03-21 16:44:44 +0800

    Updated.

--- ansible/run.sh
+++ ansible/run.sh
@@ -4,10 +4,8 @@
 MESSAGE=$(ANSIBLE_CMD)
 
 # Format output structure.
-MESSAGE="${MESSAGE//$*/}"
-MESSAGE="${MESSAGE//$\"/}"
-MESSAGE="${MESSAGE//$\'/}"
-MESSAGE="${MESSAGE//$\n/<br>}"
+MESSAGETXT="$(echo MESSAGE | sed 's/\*//g' | sed 's/\"//g')"
+MESSAGETXT="${MESSAGETXT//$'\n'/<br>}"
 
 # Send output to Teams.
-curl -X POST -H "Content-Type: application/json" -d "{\"@context\": \"http://schema.org/extensions\",\"@type\": \"MessageCard\", \"title\":\"ANSIBLE_TITLE\", \"text\": \"${MESSAGE}\"}" "ANSIBLE_WEBHOOK_URL"
+curl -X POST -H "Content-Type: application/json" -d "{\"@context\": \"http://schema.org/extensions\",\"@type\": \"MessageCard\", \"title\":\"ANSIBLE_TITLE\", \"text\": \"${MESSAGETXT}\"}" "ANSIBLE_WEBHOOK_URL"
